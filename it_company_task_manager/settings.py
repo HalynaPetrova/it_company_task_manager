4b48623c7ec528a84bc3d3ea09338d55e919025d
Halyna <1208petrova@gmail.com>
2023-10-09 19:47:05 +0300

    Create temlate navigation

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,4 +142,3 @@ ASSETS_ROOT = "/static/assets"
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
